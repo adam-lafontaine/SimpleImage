207cb3eeba7aeabfd0ea807a2df1862472400fae
adam-lafontaine <adam.lafontaine@gmail.com>
2022-12-27 08:25:23 -0500

    no memory leaks

--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -11,7 +11,7 @@ static bool test_success()
     return 
         //directory_files_test() &&
         //execute_tests() &&
-        memory_buffer_tests() &&
+        //memory_buffer_tests() &&
         //stb_simage_tests() &&
         //create_image_tests() &&
         //make_view_tests() &&
@@ -19,7 +19,7 @@ static bool test_success()
         //map_rgb_tests() &&
         //map_rgb_hsv_tests() &&
         //map_rgb_yuv_tests() &&
-        //sub_view_tests() &&
+        sub_view_tests() &&
         true;
 }
 

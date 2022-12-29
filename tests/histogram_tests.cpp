4481c7b02a0c6f89ccec465e265ef0ae3e833371
adam-lafontaine <adam.lafontaine@gmail.com>
2022-12-29 13:28:21 -0500

    fix draw histograms

--- tests/histogram_tests.cpp
+++ tests/histogram_tests.cpp
@@ -15,16 +15,22 @@ static void fill_to_top(img::View1r32 const& view, r32 value, u8 color)
     assert(value >= 0.0f);
     assert(value <= 1.0f);
 
-    auto top = view.height * (1.0f - value);
-
-    if (top >= 1.0f)
-    {
-        return;
-    }
+    int y_begin = (int)(view.height * (1.0f - value));
 
     auto r = make_range(view);
 
-    r.y_begin = top <= 0.0f ? 0u : (u32)top;
+    if (y_begin < 0)
+    {
+        r.y_begin = 0;
+    }
+    else if (y_begin >= view.height)
+    {
+        return;
+    }
+    else
+    {
+        r.y_begin = (u32)y_begin;
+    }    
 
     img::fill(img::sub_view(view, r), color);
 }
@@ -147,9 +153,7 @@ static bool histogram_fill_test()
     do_hist("blue.bmp");
 
     img::fill(view, img::to_pixel(255, 255, 255));
-    do_hist("white.bmp");
-
-    
+    do_hist("white.bmp");    
 
     img::destroy_image(hist_image);
     img::destroy_image(image);
@@ -168,25 +172,29 @@ static bool histogram_images_test()
 	auto const write_image = [&out_dir](auto const& image, const char* name) 
         { img::write_image(image, out_dir / name); };
 
-    u32 block_width = 5;
-    u32 block_space = 1;
-    u32 block_height = 105;
+    u32 width = 256 * (BIN_WIDTH + BIN_SPACE) - BIN_SPACE + 2 * HIST_SPACE;
+    u32 height = 9 * (HIST_HEIGHT + HIST_SPACE) + HIST_SPACE;
 
-    u32 width = 256 * (block_width + block_space) + block_space;
-    u32 height = 9 * block_height;
-
-    GrayImage image;
-    img::create_image(image, width, height);
-    auto view = img::make_view(image);
+    GrayImage hist_image;
+    img::create_image(hist_image, width, height);
+    auto dst = img::make_view(hist_image);
 
     img::Buffer32 buffer;
     mb::create_buffer(buffer, width * height);
 
     auto hist_view = img::make_view_1(width, height, buffer);
 
-    img::HistRGB h_rgb = { 0 };
-    img::HistHSV h_hsv = { 0 };
-    img::HistYUV h_yuv = { 0 };
+    img::HistRGB h_rgb;
+    img::HistHSV h_hsv;
+    img::HistYUV h_yuv;
+
+    auto const do_hist = [&](Image const& image, const char* filename)
+    {
+        img::histograms(img::make_view(image), h_rgb, h_hsv, h_yuv);
+        draw(h_rgb, h_hsv, h_yuv, hist_view);
+        img::map(hist_view, dst);
+        write_image(hist_image, filename);
+    };
 
     Image vette;
     img::read_image_from_file(CORVETTE_PATH, vette);
@@ -197,13 +205,12 @@ static bool histogram_images_test()
     Image chess;
     img::read_image_from_file(CHESS_PATH, chess);
 
-    img::histograms(img::make_view(vette), h_rgb, h_hsv, h_yuv);
-    draw(h_rgb, h_hsv, h_yuv, hist_view);
-    img::map(hist_view, view);
-    write_image(image, "vette.bmp");
+    do_hist(vette, "vette.bmp");
+    do_hist(caddy, "caddy.bmp");
+    do_hist(chess, "chess.bmp");
 
 
-    img::destroy_image(image);
+    img::destroy_image(hist_image);
     mb::destroy_buffer(buffer);
     img::destroy_image(vette);
     img::destroy_image(caddy);
@@ -217,8 +224,9 @@ bool histogram_tests()
 {
     printf("\n*** histogram tests ***\n");
 
-    auto result = 
-        histogram_fill_test();
+    auto result =
+        histogram_fill_test() &&
+        histogram_images_test();
 
     if (result)
     {

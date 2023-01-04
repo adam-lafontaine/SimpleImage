6ed1ee63ff6230d51d5dddf09db240b313d6c9e1
adam-lafontaine <adam.lafontaine@gmail.com>
2023-01-03 21:32:23 -0500

    add window icon

--- src/sdl/sdl_include.hpp
+++ src/sdl/sdl_include.hpp
@@ -186,35 +186,35 @@ static void close_game_controllers(SDLControllerInput& sdl, Input const& input)
 static void set_window_icon(SDL_Window* window)
 {
     // this will "paste" the struct my_icon into this function
-//#include "../resources/icon_64.h"
-//
-//// these masks are needed to tell SDL_CreateRGBSurface(From)
-//// to assume the data it gets is byte-wise RGB(A) data
-//    Uint32 rmask, gmask, bmask, amask;
-//#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-//    int shift = (window_icon.bytes_per_pixel == 3) ? 8 : 0;
-//    rmask = 0xff000000 >> shift;
-//    gmask = 0x00ff0000 >> shift;
-//    bmask = 0x0000ff00 >> shift;
-//    amask = 0x000000ff >> shift;
-//#else // little endian, like x86
-//    rmask = 0x000000ff;
-//    gmask = 0x0000ff00;
-//    bmask = 0x00ff0000;
-//    amask = (window_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
-//#endif
-//
-//    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom(
-//        (void*)window_icon.pixel_data,
-//        window_icon.width,
-//        window_icon.height,
-//        window_icon.bytes_per_pixel * 8,
-//        window_icon.bytes_per_pixel * window_icon.width,
-//        rmask, gmask, bmask, amask);
-//
-//    SDL_SetWindowIcon(window, icon);
-//
-//    SDL_FreeSurface(icon);
+#include "../resources/icon_64.h"
+
+// these masks are needed to tell SDL_CreateRGBSurface(From)
+// to assume the data it gets is byte-wise RGB(A) data
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (window_icon.bytes_per_pixel == 3) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (window_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+
+    SDL_Surface* icon = SDL_CreateRGBSurfaceFrom(
+        (void*)window_icon.pixel_data,
+        window_icon.width,
+        window_icon.height,
+        window_icon.bytes_per_pixel * 8,
+        window_icon.bytes_per_pixel * window_icon.width,
+        rmask, gmask, bmask, amask);
+
+    SDL_SetWindowIcon(window, icon);
+
+    SDL_FreeSurface(icon);
 }
 
 

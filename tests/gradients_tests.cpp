33d3b316319db158b943c7bf849f59eeb2288cf3
adam-lafontaine <adam.lafontaine@gmail.com>
2023-01-31 06:33:19 -0500

    update makefile

--- tests/gradients_tests.cpp
+++ tests/gradients_tests.cpp
@@ -1,5 +1,7 @@
 #include "tests_include.hpp"
 
+#include <cmath>
+
 
 bool gradients_xy_test()
 {
--- tests/tests_main.cpp
+++ tests/tests_main.cpp
@@ -13,18 +13,18 @@ static bool test_success()
         //execute_tests() &&
         //memory_buffer_tests() &&
         //stb_simage_tests() &&
-        create_image_tests() &&
-        make_view_tests() &&
+        //create_image_tests() &&
+        //make_view_tests() &&
         //map_tests() &&
-        //map_rgb_tests() &&
-        //map_rgb_hsv_tests() &&
-        //map_rgb_lch_tests() &&
-        //map_rgb_yuv_tests() &&        
-        sub_view_tests() &&
+        map_rgb_tests() &&
+        map_rgb_hsv_tests() &&
+        map_rgb_lch_tests() &&
+        map_rgb_yuv_tests() &&        
+        //sub_view_tests() &&
         //fill_tests() &&
         //copy_tests() &&
-        //histogram_tests() &&
-        //gradients_tests() &&
+        histogram_tests() &&
+        gradients_tests() &&
         true;
 }
 

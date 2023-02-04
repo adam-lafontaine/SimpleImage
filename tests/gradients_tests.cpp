81430228128bdaccdeb2954fb3257536c3e59e47
adam-lafontaine <adam.lafontaine@gmail.com>
2023-02-04 16:44:21 -0500

    update makefile

--- tests/gradients_tests.cpp
+++ tests/gradients_tests.cpp
@@ -80,7 +80,6 @@ bool edges_test()
 	img::transform(dst_xy, src, [](r32 grad_x, r32 grad_y) { return std::hypotf(grad_x, grad_y) > 0.1f ? 1.0f : 0.0f; });
 
 	img::map(src, view);
-
 	write_image(image, "edges.bmp");
 
 	mb::destroy_buffer(buffer);

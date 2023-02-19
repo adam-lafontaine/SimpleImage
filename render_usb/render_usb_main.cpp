f6c21d561b4422e31689484522598f4237b346fc
adam-lafontaine <adam.lafontaine@gmail.com>
2023-02-18 20:03:24 -0500

    open window

--- render_usb/render_usb_main.cpp
+++ render_usb/render_usb_main.cpp
@@ -1,49 +1,18 @@
 #include "../src/util/execute.hpp"
 #include "../src/app/app.hpp"
-#include "tests_def.hpp"
 
 //#define LEAK_CHECK
 #if defined(_WIN32) && defined(_DEBUG) && defined(LEAK_CHECK)
 #include "../src/util/win32_leak_check.h"
 #endif
 
-
-constexpr auto APP_TITLE = "SimpleImage SDL2 Tests";
+constexpr auto APP_TITLE = "Render USB";
 constexpr auto APP_VERSION = "1.0";
 
-
-static std::vector<std::function<void(img::View const&)>> tests =
+static std::vector<std::function<void(img::View const&)>> list = 
 {
-	fill_platform_view_test,
-	copy_image_test,
-	resize_image_test,
-	histogram_image_test,
-	camera_test,
-	camera_callback_test,
-	camera_histogram_test,
-	camera_continuous_test,
+
 };
-
-
-static void run_selected_test(Input const& input, app::AppState& app_state)
-{
-	static int test_id = -1;	
-
-	if (!input.keyboard.space_key.pressed)
-	{
-		return;
-	}
-
-	test_id++;
-
-	if (test_id >= tests.size())
-	{
-		test_id = 0;
-	}
-
-	tests[test_id](app_state.screen_pixels);
-}
-
 
 int main()
 {
@@ -68,7 +37,7 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	render_run(app_state, [&](auto const& input) { run_selected_test(input, app_state); });
+	render_run(app_state, [&](auto const& input) {  });
 
 	return EXIT_SUCCESS;
 }

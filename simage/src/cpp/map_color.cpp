cca235b94e36c9284f6d8983d2742cbe4d566cfb
adam-lafontaine <adam.lafontaine@gmail.com>
2023-08-26 09:37:26 -0400

    map hsv

--- simage/src/cpp/map_color.cpp
+++ simage/src/cpp/map_color.cpp
@@ -1,3 +1,5 @@
+/* yuv */
+
 namespace simage
 {
     static inline void map_span_yuv_rgb(YUV2u8* src, Pixel* dst, u32 len)
@@ -75,6 +77,60 @@ namespace simage
 }
 
 
+/* hsv */
+
+namespace simage
+{
+    static inline void map_span_rgb_hsv(Pixel* src, HSVf32p const& dst, u32 len)
+    {
+        for (u32 i = 0; i < len; ++i)
+		{
+			auto rgba = src[i].rgba;
+			auto hsv = hsv::f32_from_rgb_u8(rgba.red, rgba.green, rgba.blue);
+			dst.H[i] = hsv.hue;
+			dst.S[i] = hsv.sat;
+			dst.V[i] = hsv.val;
+		}
+    }
+
+
+    static inline void map_span_hsv_rgba(HSVf32p const& src, Pixel* dst, u32 len)
+	{
+		for (u32 i = 0; i < len; ++i)
+		{
+			auto rgb = hsv::f32_to_rgb_u8(src.H[i], src.S[i], src.V[i]);
+			dst[i] = to_pixel(rgb.red, rgb.green, rgb.blue);
+		}
+	}
+
+
+    static inline void map_span_rgb_hsv(RGBf32p const& src, HSVf32p const& dst, u32 len)
+	{
+		for (u32 i = 0; i < len; ++i)
+		{
+			auto hsv = hsv::f32_from_rgb_f32(src.R[i], src.G[i], src.B[i]);
+			dst.H[i] = hsv.hue;
+			dst.S[i] = hsv.sat;
+			dst.V[i] = hsv.val;
+		}
+	}
+
+
+	static inline void map_span_hsv_rgb(HSVf32p const& src, RGBf32p const& dst, u32 len)
+	{
+		for (u32 i = 0; i < len; ++i)
+		{
+			auto rgb = hsv::f32_to_rgb_f32(src.H[i], src.S[i], src.V[i]);
+			dst.R[i] = rgb.red;
+			dst.G[i] = rgb.green;
+			dst.B[i] = rgb.blue;
+		}
+	}
+}
+
+
+/* map_yuv */
+
 namespace simage
 {
     void map_yuv_rgba(ViewYUV const& src, View const& dst)
@@ -114,5 +170,50 @@ namespace simage
         u32 len = src.width * src.height;
 
         map_span_yuv_rgba(yuv_row_begin(src, 0), dst.data, len);
+    }
+}
+
+
+/* map_hsv */
+
+namespace simage
+{
+	void map_rgb_hsv(View const& src, ViewHSVf32 const& dst)
+    {
+        assert(verify(src, dst));
+
+        u32 len = src.width * src.height;
+
+        map_span_rgb_hsv(src.data, hsv_row_begin(dst, 0), len);
+    }
+
+
+	void map_hsv_rgba(ViewHSVf32 const& src, View const& dst)
+    {
+        assert(verify(src, dst));
+
+        u32 len = src.width * src.height;
+
+        map_span_hsv_rgba(hsv_row_begin(src, 0), dst.data, len);
+    }
+
+
+	void map_rgb_hsv(ViewRGBf32 const& src, ViewHSVf32 const& dst)
+    {
+        assert(verify(src, dst));
+
+        u32 len = src.width * src.height;
+
+        map_span_rgb_hsv(rgb_row_begin(src, 0), hsv_row_begin(dst, 0), len);
+    }
+
+
+	void map_hsv_rgb(ViewHSVf32 const& src, ViewRGBf32 const& dst)
+    {
+        assert(verify(src, dst));
+
+        u32 len = src.width * src.height;
+
+        map_span_hsv_rgb(hsv_row_begin(src, 0), rgb_row_begin(dst, 0), len);
     }
 }
